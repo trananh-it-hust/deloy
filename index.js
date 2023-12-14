6cac4cd07079e2d2ab516da56595d7b63c468b2b
trananh-it-hust <haianhtranmanh@gmail.com>
2023-12-14 17:41:50 +0700

    12

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ import { connectMongoDb, Collections } from "./database/index.js";
 const app = express();
 app.use(express.json());
 
+app.post("/register", async (req, res) => {
+  const { name, gmail, password, confirmpassword } = req.body;
+  const newuser = { name, gmail, password, confirmpassword };
+  const check = await Collections["USERS"].findOne({ gmail });
+  if (check) {
+    res.send({ msg: "Thất bại" });
+    return;
+  } else {
+    const createuser = await Collections["USER"].insertOne(newuser);
+    res.send({ msg: "Thành công", data: createuser });
+  }
+});
 app.get("/", (req, res) => {
   res.send("Thành công");
 });
 app.listen(8004, () => {
   console.log("Sever is running !");
+  connectMongoDb();
 });
