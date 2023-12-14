5cafd30cc9c7761dae012bfc2270a6c605e91ca7
trananh-it-hust <haianhtranmanh@gmail.com>
2023-12-14 17:21:37 +0700

    third

--- database/index.js
+++ database/index.js
@@ -3,7 +3,9 @@ import mongodb from "mongodb";
 const Collections = {};
 
 const connectMongoDb = async () => {
-  const client = new mongodb.MongoClient("mongodb://127.0.0.1:27017");
+  const client = new mongodb.MongoClient(
+    "mongodb+srv://admin:anhpchy117@cluster0.z622hiu.mongodb.net/?retryWrites=true&w=majority"
+  );
   await client.connect();
   const db = client.db("exam");
   Collections["USERS"] = db.collection("users");
